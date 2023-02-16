1a081a3926aba6c350eec518fa7c2f90a622dd2d
Kravch773 <ivankravchk0@gmail.com>
2023-02-16 17:38:18 +0200

    test11

--- wdio.conf_2.ts
+++ wdio.conf_2.ts
@@ -31,7 +31,7 @@ export const config: Options.Testrunner = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.ts'
+        './test/specs/**/test1.ts'
     ],
     suites: {
         test1: [
