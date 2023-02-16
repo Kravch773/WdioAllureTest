df46941b73a2a2abb4ad2c3173835f4dc9602d87
Kravch773 <ivankravchk0@gmail.com>
2023-02-16 13:10:38 +0200

    test3

--- test/specs/test2.ts
+++ test/specs/test2.ts
@@ -3,12 +3,14 @@ import SecurePage from '../pageobjects/secure.page.js'
 
 describe('test 2', () => {
     it('test 2', async () => {
-        await LoginPage.open()
+        await browser.url("https://www.google.com/")
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!')
+        const p = $('//input');
+        p.setValue("pepega");
+        (await $('(//input[@name="btnK"])[2]')).click();
+        
+        await expect($("//div//h3").getText()).toContain("pepega");
+
     })
 })
 
