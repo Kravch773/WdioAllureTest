68dc40f58bdaf94993e8bae253e52fef6552f2a4
Kravch773 <ivankravchk0@gmail.com>
2023-02-16 16:51:55 +0200

    test5

--- test/specs/test2.ts
+++ test/specs/test2.ts
@@ -10,6 +10,26 @@ describe('test 2', () => {
         expect(await browser.getTitle()).toBe("pepega")
     
     })
+    it('test 3', async () => {
+        await browser.url("https://www.google.com/")
+
+        const p = $('//input');
+        p.setValue("pepega");
+        (await $('(//input[@name="btnK"])[2]')).click();
+        console.log("test2-"+await browser.getTitle())
+        expect(await browser.getTitle()).toBe("Google")
+    
+    })
+    it('test 4', async () => {
+        await browser.url("https://www.google.com/")
+
+        const p = $('//input');
+        p.setValue("pepega");
+        (await $('(//input[@name="btnK"])[2]')).click();
+        console.log("test2-"+await browser.getTitle())
+        expect(await browser.getTitle()).toBe("Google")
+    
+    })
 })
 
 
